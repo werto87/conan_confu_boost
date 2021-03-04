b2e825ce1c3f8568c086dfe020f2faef8c3395d4
werto87 <werto87>
2021-03-04 22:19:51 +0000

    use main head

--- conanfile.py
+++ conanfile.py
@@ -16,30 +16,30 @@ class ConfuBoostConan(ConanFile):
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
     generators = "cmake"
+    scm = {
+        "type": "git",
+        "subfolder": "confu_boost",
+        "url": "https://github.com/werto87/confu_boost.git",
+        "revision": "main"
+    }
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
-    
+
     def requirements(self):
         self.requires("boost/1.75.0")
         self.requires("magic_enum/0.6.6")
         self.requires("catch2/2.13.1")
 
-    def source(self):
-        zip_name = "confu_boost"
-        download("https://github.com/werto87/confu_boost/archive/v0.0.1.zip", zip_name)
-        unzip(zip_name)
-        os.unlink(zip_name)
-
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_folder="confu_boost-0.0.1")
+        cmake.configure(source_folder="confu_boost")
         cmake.build()
 
     def package(self):
         self.copy("*.h*", dst="include/confu_boost",
-                  src="confu_boost-0.0.1/confu_boost")
+                  src="confu_boost/confu_boost")
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
